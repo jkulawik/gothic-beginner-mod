cac6495a530a401ebac8d0b0375c7452e2254ade
jacob <jacob@samochud.home>
2023-02-06 16:05:49 +0100

    itemfeller for mod as player char

---
+++ _work/Data/Scripts/Content/MyMod/Story/NPC/DEV_rockefeller.d
@@ -0,0 +1,129 @@
+INSTANCE DEV_Rockefeller(NPC_DEFAULT)
+// PlayerInstanz
+{
+	//-------- primary data --------
+	name 			= "Mod Rockefeller";
+	Npctype			= NPCTYPE_MAIN;
+	guild			= GIL_NONE;
+	level			= 100;
+	voice			= 15;
+	id			= 0;
+	
+	//--------- abilities --------
+	attribute[ATR_STRENGTH]	 	=	1000;
+	attribute[ATR_DEXTERITY]	=	1000;
+	attribute[ATR_MANA_MAX]		=	1000;
+	attribute[ATR_MANA]		=	1000;
+	attribute[ATR_HITPOINTS_MAX]	=	1000;
+	attribute[ATR_HITPOINTS]	=	1000;
+	
+	exp				=	(500*((level+1)/2)*(level+1));
+	exp_next			=	(500*((level+2)/2)*(level+1));
+
+	//-------- visuals --------
+	// 						animations
+	Mdl_SetVisual		(self,"HUMANS.MDS");
+	//			Body-Mesh	Body-Tex	Skin-Color	Head-MMS    Head-Tex	Teeth-Tex 	Armor-Tex
+	Mdl_SetVisualBody (self, "hum_body_Naked0", BodyTex_Player, 0, "Hum_Head_Pony", FACE_N_Player, 0, ITAR_Mod_FakeRaven);
+	
+	//-------- Skills --------
+	Npc_SetTalentSkill	(self, NPC_TALENT_MAGE, 			6);
+	Npc_SetTalentSkill	(self, NPC_TALENT_PICKLOCK, 			1); //hängt ab von DEX (auf Programmebene)
+	Npc_SetTalentSkill	(self, NPC_TALENT_SNEAK, 			1);
+	Npc_SetTalentSkill	(self, NPC_TALENT_ACROBAT, 			0);
+	
+	Npc_SetTalentSkill	(self, NPC_TALENT_PICKPOCKET, 			1);	//hängt ab von DEX (auf Scriptebene)
+	Npc_SetTalentSkill	(self, NPC_TALENT_SMITH, 			1);
+	Npc_SetTalentSkill	(self, NPC_TALENT_RUNES, 			1);
+	Npc_SetTalentSkill	(self, NPC_TALENT_ALCHEMY, 			1);
+	Npc_SetTalentSkill	(self, NPC_TALENT_TAKEANIMALTROPHY,		1);
+	
+	PLAYER_TALENT_ALCHEMY[POTION_Health_01]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Health_02]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Health_03]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Mana_01]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Mana_02]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Mana_03]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Speed]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Perm_STR]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Perm_DEX]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Perm_Mana]		= TRUE; 
+	PLAYER_TALENT_ALCHEMY[POTION_Perm_Health]	= TRUE; 
+
+	PLAYER_TALENT_SMITH[WEAPON_Common] 		= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_1H_Special_01] 	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_2H_Special_01] 	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_1H_Special_02] 	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_2H_Special_02]	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_1H_Special_03] 	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_2H_Special_03] 	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_1H_Special_04] 	= TRUE;
+	PLAYER_TALENT_SMITH[WEAPON_2H_Special_04] 	= TRUE;
+
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Teeth] 			= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Claws]			= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Fur] 			= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Heart] 			= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_ShadowHorn] 		= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_FireTongue] 		= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_BFWing] 			= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_BFSting] 			= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_Mandibles] 		= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_CrawlerPlate] 		= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DrgSnapperHorn] 		= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DragonScale] 		= TRUE;
+	PLAYER_TALENT_TAKEANIMALTROPHY[TROPHY_DragonBlood] 		= TRUE;
+	
+	PLAYER_TALENT_RUNES[SPL_LIGHT] 					= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Firebolt] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Icebolt] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_LightHeal] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_SummonGoblinSkeleton] 			= TRUE;
+	PLAYER_TALENT_RUNES[SPL_InstantFireball] 			= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Zap] 					= TRUE;
+	PLAYER_TALENT_RUNES[SPL_SummonWolf] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_WINDFIST] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Sleep] 					= TRUE;
+	PLAYER_TALENT_RUNES[SPL_MediumHeal] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_LightningFlash] 			= TRUE;
+	PLAYER_TALENT_RUNES[SPL_ChargeFireball] 			= TRUE;
+	PLAYER_TALENT_RUNES[SPL_SummonSkeleton] 			= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Fear] 					= TRUE;
+	PLAYER_TALENT_RUNES[SPL_IceCube] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_ChargeZap] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_SummonGolem] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_DestroyUndead] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Pyrokinesis] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Firestorm] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_IceWave] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_SummonDemon] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_FullHeal] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Firerain] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_BreathOfDeath] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_MassDeath] 				= TRUE;
+	PLAYER_TALENT_RUNES[SPL_ArmyOfDarkness] 			= TRUE;
+	PLAYER_TALENT_RUNES[SPL_Shrink] 				= TRUE;
+	
+	B_SetFightSkills (self, 100); //Grenzen für Talent-Level liegen bei 30 und 60
+	
+	//bodyStateInterruptableOverride = TRUE;
+	
+	//-------- inventory --------
+	
+	EquipItem (self, ItMw_Mod_2H_FakeSword);
+
+
+//	CreateInvItem(self,ItMw_Mod_2H_FakeSword);
+	
+//	CreateInvItem(self,ITAR_Mod_FakeRaven);
+
+};
+
+
+
+
+
+
+
+
+
