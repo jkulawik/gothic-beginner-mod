9f6acf3fe0a3f53135ef08c9b5fd5580e730255e
jacob <jacob@samochud.home>
2023-02-06 15:24:33 +0100

    gold raven armor wip

---
+++ _work/Data/Scripts/Content/MyMod/Items/IT_Mod_Armors.d
@@ -0,0 +1,31 @@
+INSTANCE ITAR_Mod_FakeRaven (C_Item)
+{
+	name 					=	"Ozdobna złota zbroja";
+
+	mainflag 				=	ITEM_KAT_ARMOR;
+	flags 					=	0;
+
+	protection [PROT_EDGE]	=	30;
+	protection [PROT_BLUNT] = 	30;
+	protection [PROT_POINT] = 	30;
+	protection [PROT_FIRE] 	= 	0;
+	protection [PROT_MAGIC] = 	0;
+
+	value 					=	VALUE_ITAR_Raven_Addon;
+
+	wear 					=	WEAR_TORSO;
+
+	visual 					=	"ItAr_Raven_ADDON.3ds";
+	visual_change 				=	"Armor_Raven_ADDON.asc";
+	visual_skin 				=	0;
+	material 				=	MAT_METAL;
+
+	description				=	name;
+
+	TEXT[0]					=	"Jest wykonana z cienkiej blachy.";
+	TEXT[1]					=	NAME_Prot_Edge;		COUNT[1]	= 	protection[PROT_EDGE];
+	TEXT[2]					=	NAME_Prot_Point;	COUNT[2]	= 	protection[PROT_POINT];
+	TEXT[3] 				=	NAME_Prot_Fire;		COUNT[3]	= 	protection[PROT_FIRE];
+	TEXT[4]					=	NAME_Prot_Magic;	COUNT[4]	= 	protection[PROT_MAGIC];
+	TEXT[5]					=	NAME_Value;		COUNT[5]	= 	value;
+};
--- _work/Data/Scripts/Content/MyMod/Items/IT_Mod_Weapons.d
+++ _work/Data/Scripts/Content/MyMod/Items/IT_Mod_Weapons.d
@@ -1,6 +1,6 @@
 INSTANCE ItMw_Mod_2H_FakeSword (C_Item) 
-{	
-	name 				=	"Zardzewia³y miecz dwurêczny";
+{
+	name 				=	"Œwiec¹cy z³oty miecz";
 
 	mainflag 			=	ITEM_KAT_NF;
 	flags 				=	ITEM_2HD_SWD;	
@@ -12,13 +12,61 @@ INSTANCE ItMw_Mod_2H_FakeSword (C_Item)
 	damagetype			=	DAM_EDGE;
 	range    			=  	Range_Mod_2H_FakeSword;		
 
-	cond_atr[2]   		= 	ATR_STRENGTH;
-	cond_value[2]  		= 	Condition_Mod_2H_FakeSword;
+	cond_atr[2]   			= 	ATR_STRENGTH;
+	cond_value[2]  			= 	Condition_Mod_2H_FakeSword;
 	visual 				=	"golden_uriziel.3DS";
 
+	wear				=	WEAR_EFFECT;
+	effect				=	"spellFX_crossbow";
+
 	description			= name;
-	TEXT[2]				= NAME_Damage;					COUNT[2]	= damageTotal;
-	TEXT[3] 			= NAME_Str_needed;				COUNT[3]	= cond_value[2];
+	TEXT[0]				= "Wygl¹da groŸnie, ale nie jest najlepszej jakoœci.";
+	TEXT[1] 			= "";
+	TEXT[2]				= NAME_Damage;				COUNT[2]	= damageTotal;
+	TEXT[3] 			= NAME_Str_needed;			COUNT[3]	= cond_value[2];
 	TEXT[4] 			= NAME_TwoHanded;
+	TEXT[5]				= NAME_Value;				COUNT[5]	= value;
+};
+
+
+
+/*
+INSTANCE itmw_Uriziel  (C_Item) 
+{	
+	name 				=	"Uriziel";
+
+	mainflag 			=	ITEM_KAT_NF;
+	flags 				=	ITEM_2HD_SWD;	
+	material 			=	MAT_METAL;
+
+	value 				=	10000000;
+
+//	damage		[DAM_INDEX_BLUNT]	=	0;
+	damage		[DAM_INDEX_EDGE]	=	50;
+//	damage		[DAM_INDEX_POINT]	=	0;
+	damage		[DAM_INDEX_FIRE]	=	75;
+	damage		[DAM_INDEX_FLY]		=	80; 
+	damage		[DAM_INDEX_MAGIC]	=	45;
+
+	damagetype			=	DAM_EDGE|DAM_FLY|DAM_FIRE|DAM_MAGIC;
+	range    			=  	Range_Special_2H_4;	
+	
+	on_equip			=	Equip_2H_10;
+	on_unequip			=	UnEquip_2H_10;
+	
+	cond_atr[2]   		= 	ATR_STRENGTH;
+	cond_value[2]  		= 	0;
+	visual 				=	"ITMW_2H_SWORD_SLEEPER_02.3DS";
+
+	wear				=	WEAR_EFFECT;
+	effect				=	"spellFX_crossbow";
+
+	description			= name;
+	TEXT[2]				= NAME_Damage;                                  COUNT[2]	= 250;
+	TEXT[1] 			= "Es lässt seine Gegner in Flammen aufgehen.";
+	TEXT[4]				= NAME_ADDON_BONUS_2H;			        COUNT[4]	= Waffenbonus_10;
+	TEXT[3]				= NAME_ADDON_ONEHANDED_BELIAR;			COUNT[3]	= 80;
+	TEXT[0]				= "Mit diesem Schwert habe ich den Schläfer verbannt.";	
 	TEXT[5]				= NAME_Value;					COUNT[5]	= value;
 };
+*/
