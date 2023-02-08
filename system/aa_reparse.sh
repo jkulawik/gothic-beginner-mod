af4d6b796c0a7c13cad4d999dff6e7c3390d0f76
jacob <jacob@samochud.home>
2023-02-08 01:27:39 +0100

    another reparse variant

--- system/aa_reparse.sh
+++ system/aa_reparse.sh
@@ -1,5 +1,8 @@
 # Full test 
 # WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -zReparse_OU -zReparse_Menu -nomenu
 
+# No menu reparse
+WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -zReparse_OU -nomenu
+
 # NPC and Item test (no dialogues)
-WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -nomenu
+# WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -nomenu
