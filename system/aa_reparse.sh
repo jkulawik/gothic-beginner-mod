897a8c22adcd2d15bad7747663143d729fd9d69e
jacob <jacob@samochud.home>
2023-02-08 12:51:26 +0100

    improved launch script with choices

--- system/aa_reparse.sh
+++ system/aa_reparse.sh
@@ -1,8 +1,16 @@
-# Full test 
-# WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -zReparse_OU -zReparse_Menu -nomenu
 
-# No menu reparse
-WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -zReparse_OU -nomenu
-
-# NPC and Item test (no dialogues)
-# WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -nomenu
+if [[ $1 == "1" ]]
+then
+	echo "Full test"
+	WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -zReparse_OU -zReparse_Menu -nomenu
+elif [[ $1 == "2" ]]
+then
+	echo "No menu reparse"
+	WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -zReparse_OU -nomenu
+elif [[ $1 == "3" ]]
+then
+	echo "NPC and Item test (no dialogues)"
+	WINEPREFIX="/home/jacob/Games/Gothic II/wine_prefix" wine Gothic2.exe -GAME:aTestMod.ini -zReparse_Game -nomenu
+else
+	echo "Enter argument 1. Options are: 1-3"
+fi
